3cf839adc0558c70c386b8e1c3f3f9edea7d4c5e
ainu90 <ainu90@gmail.com>
2023-06-14 08:54:19 +0900

    Exam/

--- Exam/src/test1/Test8.java
+++ Exam/src/test1/Test8.java
@@ -6,11 +6,11 @@ package test1;
  */
 public class Test8 {
  public static void main(String[] args) {
-	int n=5;
+	int n=5; // n= 첫줄
 	
 	for (int i=0; i<n; i++) {
 		
-		for (int j=n-1;j>i;j-- ) {
+		for (int j=n-1;j>i;j-- ) { //
 			System.out.print(" ");
 		}
 		for(int j=0 ; j<=2*i;j++) {
--- Exam/src/test1/Test9.java
+++ Exam/src/test1/Test9.java
@@ -10,9 +10,9 @@ public static void main(String[] args) {
 	int count = 0;
 	for (int i=1; i<=9; i++) {
 		if(i<=5) {
-			
+			count+=1;
 		}else {
-			
+			count-=1;
 		}
 		
 	for (int j=1 ; j<=5-count ; j++) {
