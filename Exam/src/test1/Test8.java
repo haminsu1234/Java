aefd39b325ef398bd42950e2f65a60a1afe97c71
ainu90 <ainu90@gmail.com>
2023-06-13 17:36:28 +0900

    예제추가

--- Exam/src/test1/Test8.java
+++ Exam/src/test1/Test8.java
@@ -10,10 +10,10 @@ public class Test8 {
 	
 	for (int i=0; i<n; i++) {
 		
-		for (int j=n-1;j>=n-i;j-- ) {
+		for (int j=n-1;j>i;j-- ) {
 			System.out.print(" ");
 		}
-		for(int j=0 ; j<=(i*2-1);j++) {
+		for(int j=0 ; j<=2*i;j++) {
 			System.out.print("★");
 		}
 
--- Exam/src/test1/Test9.java
+++ Exam/src/test1/Test9.java
@@ -2,8 +2,29 @@ package test1;
 /**
  * 날짜 :2023/06/13
  * 이름 : 하민수
- *
+ * 예제 9
  */
 public class Test9 {
-
+public static void main(String[] args) {
+	
+	int count = 0;
+	for (int i=1; i<=9; i++) {
+		if(i<=5) {
+			
+		}else {
+			
+		}
+		
+	for (int j=1 ; j<=5-count ; j++) {
+		System.out.print(" ");
+	}
+	for (int k=1 ; k<=2*count-1 ; k++) {
+		System.out.print("*");
+		
+	}
+	System.out.print("\n");
+	
+	}
+		
+}
 }
