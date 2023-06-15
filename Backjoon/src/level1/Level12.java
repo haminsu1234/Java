020d39977cddc8cd7e196b5dd9480c90a53fae30
ainu90 <ainu90@gmail.com>
2023-06-15 22:22:39 +0900

    조건문 3번까지

---
+++ Backjoon/src/level1/Level12.java
@@ -0,0 +1,16 @@
+package level1;
+
+
+
+public class Level12 {
+	public static void main(String[] args) {
+	
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+	}
+
+}
