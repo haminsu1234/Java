8e0b4e18644ddb0e72c81910de6dc1dfdd427feb
ainu90 <ainu90@gmail.com>
2023-07-19 17:48:02 +0900

    추가

--- Backjoon/src/level8/Level02.java
+++ Backjoon/src/level8/Level02.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Level01 {
+public class Level02 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		int str =Integer.parseInt(st.nextToken());		
 		int n =Integer.parseInt(st.nextToken());
 		String change =Integer.toString(str, n);
-		System.out.println(change);
+		System.out.println(change.toUpperCase());
 
 
 		
---
+++ Backjoon/src/level8/Level03.java
@@ -0,0 +1,65 @@
+package level8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Level03 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		//quarter 0.25 , dime 0.10 nickel 0.05 penny 0.01
+		int t = Integer.parseInt(br.readLine());
+		int c;
+		int quarter=0;
+		int dime=0;
+		int nickel=0;
+		int penny=0;
+		
+		for(int i=0; i<t;i++) {
+			c=Integer.parseInt(br.readLine());
+			
+			if(c>=25) {
+				quarter=c/25;
+				if(c%25>=10) {
+					dime= (c%25)/10;
+					if((c%25)%10>=5) {
+						nickel=(c%25)/5;
+					}else {
+						penny=(c%25)/1;
+					}
+				}else if(c%25>=5) {
+					nickel=(c%25)/5;
+					if()
+				}else {
+					penny=(c%25)/1;
+				}
+				
+
+			}else if(c>=10) {
+				dime=c/10;
+				if(c%10>=5) {
+					nickel=(c%10)/5;
+				}else {
+					penny=(c%10)/1;
+
+				}
+			}else if(c>=5) {
+				nickel=c/5;
+				if(c%5>=1) {
+					penny=(c%5)/1;
+
+				}
+
+			}else {
+				penny=c/1;
+
+			}
+			System.out.println(quarter+" "+dime+" "+nickel+" "+penny);
+		}
+
+
+
+		
+	}
+}
